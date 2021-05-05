d89a80037c1dd0a8feb71e460901581820dc6a3e
Ula028 <utalalaj1@sheffield.ac.uk>
2021-05-05 14:57:59 +0100

    Install and activate the service worker

---
+++ public/sw.js
@@ -0,0 +1,34 @@
+var CACHE_NAME = 'canvas-cache-v1';
+var urlsToCache = [
+  '/',
+  '/javascripts/canvas.js',
+  '/javascripts/index.js',
+  '/stylesheets/style.css'
+];
+
+// installation of service worker
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function (cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// activation of service worker, removing old cache
+self.addEventListener('activate', function (e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function (keyList) {
+      return Promise.all(keyList.map(function (key) {
+        if (key !== CACHE_NAME) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  return self.clients.claim();
+});
