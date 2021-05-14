192470f3b2c6f5325fc7305b11d10e1c3965d599
Ula028 <utalalaj1@sheffield.ac.uk>
2021-05-14 17:57:51 +0100

    Handling GET requests in service worker

--- public/sw.js
+++ public/sw.js
@@ -11,7 +11,7 @@ self.addEventListener('install', function (event) {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(function (cache) {
-        console.log('Opened cache');
+        console.log('[ServiceWorker] Opened cache');
         return cache.addAll(urlsToCache);
       })
   );
@@ -31,4 +31,46 @@ self.addEventListener('activate', function (e) {
     })
   );
   return self.clients.claim();
+});
+
+self.addEventListener('fetch', function (event) {
+  var requestURL = new URL(event.request.url)
+
+  // Handle POST requests
+  if (event.request.method == 'POST') {
+    return;
+  }
+
+  // Handle GET requests
+  event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        return fetch(event.request).then(
+          function (response) {
+            // Check if we received a valid response
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // IMPORTANT: Clone the response. A response is a stream
+            // and because we want the browser to consume the response
+            // as well as the cache consuming the response, we need
+            // to clone it so we have two streams.
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function (cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+  );
 });
